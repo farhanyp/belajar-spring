6c78eee1af79ea817bc2811de561fdbfc2664d46
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-20 06:50:57 +0700

    Change Bean Name

---
+++ belajar-spring-dasar/src/main/java/belajar/spring/belajar_spring_dasar/BeanNameConfiguration.java
@@ -0,0 +1,21 @@
+package belajar.spring.belajar_spring_dasar;
+
+import belajar.spring.belajar_spring_dasar.data.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class PrimaryBeanConfiguration {
+
+    @Primary
+    @Bean
+    public Foo foo1(){
+        return new Foo();
+    }
+
+    @Bean
+    public Foo foo2(){
+        return new Foo();
+    }
+}
