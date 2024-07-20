8d742156715ed8250ecbe1637414ed407bb4cde7
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-21 00:34:47 +0700

    Create Entity

---
+++ belajar-spring-restful-api/src/test/java/farhan_yudha_pratama/belajar_spring_restful_api/BelajarSpringRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package farhan_yudha_pratama.belajar_spring_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
