c2912a69466920dbcbc5dc54d74d5ae61f93590d
roronoa52 <farhan.yudha2016we@gmail.com>
2024-07-20 11:39:27 +0700

    Learn Resource

---
+++ belajar-spring-config-properties/src/main/java/farhan_yudha_pratama/belajar_spring_config_properties/BelajarSpringConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package farhan_yudha_pratama.belajar_spring_config_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringConfigPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringConfigPropertiesApplication.class, args);
+	}
+
+}
---
+++ belajar-spring-config-properties/src/test/java/farhan_yudha_pratama/belajar_spring_config_properties/BelajarSpringConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package farhan_yudha_pratama.belajar_spring_config_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringConfigPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
